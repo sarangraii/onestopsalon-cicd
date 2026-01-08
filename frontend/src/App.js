045d9c5600e596d2af4558c432756250f60ac818
sarangraii <sarang2452@gmail.com>
2026-01-08 18:10:12 +0530

    Add frontend as normal folder with Dockerfile

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>OneStopSalon</h1>
+      <p>Frontend Running Successfully 🚀</p>
+    </div>
+  );
+}
+
+export default App;
